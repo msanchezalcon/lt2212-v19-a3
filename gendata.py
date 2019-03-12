8cd42807386faa74aa74aff769a37ba25429b2f2
Míriam Sánchez Alcón <46067629+msanchezalcon@users.noreply.github.com>
2019-03-12 19:37:47 +0100

    Added two new functions

--- gendata.py
+++ gendata.py
@@ -10,37 +10,23 @@ import pandas as pd
 
 # add whatever additional imports you may need here. You may not use the
 # scikit-learn OneHotEncoder, or any related automatic one-hot encoders.
-from nltk import word_tokenize
+from nltk import word_tokenize, sent_tokenize, ngrams
 import re
 from sklearn.utils import shuffle
-from nltk import ngrams
-
-parser = argparse.ArgumentParser(description="Convert text to features")
-parser.add_argument("-N", "--ngram", metavar="N", dest="ngram", type=int, default=3,
-                    help="The length of ngram to be considered (default 3).")
-parser.add_argument("-S", "--start", metavar="S", dest="startline", type=int,
-                    default=0,
-                    help="What line of the input data file to start from. Default is 0, the first line.")
-parser.add_argument("-E", "--end", metavar="E", dest="endline",
-                    type=int, default=None,
-                    help="What line of the input data file to end on. Default is None, whatever the last line is.")
-parser.add_argument("inputfile", type=str,
-                    help="The file name containing the text data.")
-parser.add_argument("outputfile", type=str,
-                    help="The name of the output file for the feature table.")
 
 
 
 
-def preprocessing_data(file):
+
+def preprocessing_data(filepath):
     """
     Takes file, removes punctuation and divides it into tokens
     Stores tokens in a sorted vocabulary list
     """
-    vocab = []'0'
-    with open(file,"r",encoding="utf8") as f:
+    vocab = []
+    with open(filepath,"r",encoding="utf8") as f:
         file = f.read()
-        clean_text = re.sub(r"/[^\s]+", "", file)
+        clean_text = re.sub(r'([^\w\s\']|\n)', '', file)
         clean_text = word_tokenize(clean_text)
         for token in clean_text:
             if token not in vocab:
@@ -70,23 +56,34 @@ def vector_builder(inputfile):
 
 
 
-def split_data(file):
+def split_data(file, startline, endline):
     """
-    Splits tokens into train and test data and stores them into two separate lists.
+    Splits text (tokens) into train and test data and stores them into two separate lists.
+    We first define a chunk of text we will use and that chunk will be separated into train and test every time.
+    Always taking 80% randomly.
     """
     with open(file,"r",encoding="utf8") as f:
         file = f.read()
-        clean_text = re.sub(r"/[^\s]+", "", file)
-        clean_text = word_tokenize(clean_text)
-        train_split = 0.80
-        train_data, test_data = clean_text[:int(train_split * len(clean_text))], clean_text[int(train_split * len(clean_text)):]
-        train_data = shuffle(train_data)
-        test_data = shuffle(test_data)
+        clean_text = re.sub(r'([^\w\s\']|\n)', '', file)
+        sent_token = sent_tokenize(clean_text)
+        if startline is not None:
+            sent_token = sent_token[startline:]
+        if endline is not None:
+            sent_token = sent_token[:endline]
+    for sentence in sent_token:
+        tokens = word_tokenize(sentence)
 
-        return train_data, test_data
+    train_split = 0.80
+    train_data, test_data = tokens[:int(train_split * len(tokens))], tokens[int(train_split * len(tokens)):]
+    train_data = shuffle(train_data) # randomizes selection every time
+    test_data = shuffle(test_data)
+
+    print(tokens)
+    return train_data, test_data
 
 
-def ngrams_builder(file, n):
+
+def ngrams_builder(file, n, startline, endline):
     """
     Takes a list of tokens from the vocabulary list and returns n-grams as a list of tuples.
     """
@@ -94,27 +91,58 @@ def ngrams_builder(file, n):
        # file = f.read()
        # clean_text = re.sub(r"/[^\s]+", "", file)
         #clean_text = word_tokenize(clean_text)
-    train_data, test_data = split_data(file)
-    n_grams = ngrams(train_data, n, pad_left=True, pad_right=True, left_pad_symbol="<s>", right_pad_symbol="<e>")
-    print(list(n_grams))
+    data = split_data(file, startline, endline)
+    n_grams = ngrams(data, n, pad_left=True, left_pad_symbol="<s>")
+    #print(n_grams)
     return list(n_grams)
 
 
-def n_gram_vector():
+
+def vector_builder(ngrams, vectordict):
     """
     Takes one-hot vectors from tokens and creates n-gram vectors (list of lists). Then convert that array into
-     a dataframe.
+    a dataframe.
     """
+    main_vectors = []
+    for gram in ngrams: # each token in the n-gram
+        sub_vectors = []
+        for word in gram[:-1]:
+            sub_vectors += vectordict[word]
+        sub_vectors.append(gram[-1])
+        main_vectors.append(sub_vectors)
+    array_of_vectors = np.array(main_vectors)
+    dataframe = pd.DataFrame(array_of_vectors)
+
+    return dataframe
+
+
+def file_builder(outputfile):
+    """
+    Returns a csv version of the dataframe containing a representation of the array of vectors.
+    """
+    dataframe = vector_builder(ngrams, vectordict)
+
+    return pd.DataFrame(dataframe).to_csv(outputfile)
 
 
 
 
 
 
-preprocessing_data(args.inputfile)
-split_data(args.inputfile)
-ngrams_builder(args.inputfile, args.ngram)
-vector_builder(args.inputfile)
+
+parser = argparse.ArgumentParser(description="Convert text to features")
+parser.add_argument("-N", "--ngram", metavar="N", dest="ngram", type=int, default=3,
+                    help="The length of ngram to be considered (default 3).")
+parser.add_argument("-S", "--start", metavar="S", dest="startline", type=int,
+                    default=0,
+                    help="What line of the input data file to start from. Default is 0, the first line.")
+parser.add_argument("-E", "--end", metavar="E", dest="endline",
+                    type=int, default=None,
+                    help="What line of the input data file to end on. Default is None, whatever the last line is.")
+parser.add_argument("inputfile", type=str,
+                    help="The file name containing the text data.")
+parser.add_argument("outputfile", type=str,
+                    help="The name of the output file for the feature table.")
 
 args = parser.parse_args()
 
@@ -130,3 +158,11 @@ print("Writing table to {}.".format(args.outputfile))
 
 # THERE ARE SOME CORNER CASES YOU HAVE TO DEAL WITH GIVEN THE INPUT
 # PARAMETERS BY ANALYZING THE POSSIBLE ERROR CONDITIONS.
+
+vocabdict = preprocessing_data(args.inputfile)
+vectordict = vector_builder(args.inputfile)
+ngrams = ngrams_builder(args.inputfile, args.ngrams, args.startline, args.endline)
+vector_builder(ngrams, vectordict)
+split_data(args.inputfile, args.startline, args.endline)
+ngrams_builder(args.inputfile, args.ngram, args.startline, args.endline)
+file_builder(args.outputfile)
