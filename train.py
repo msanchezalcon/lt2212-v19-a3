ba0dd224e94634f1059cf11a5aaaad56f92c143f
Míriam Sánchez-Alcón <46067629+msanchezalcon@users.noreply.github.com>
2019-05-02 21:47:56 +0200

    Add files via upload

--- train.py
+++ train.py
@@ -1,14 +1,36 @@
-import os, sys
 import argparse
 import numpy as np
 import pandas as pd
 import pickle
+import os, sys
 from sklearn.linear_model import LogisticRegression
 
 # train.py -- Don't forget to put a reasonable amount code comments
 # in so that we better understand what you're doing when we grade!
 
 # add whatever additional imports you may need here.
+
+def read_file(dataframe):
+    """
+    Opens csv file created in gendata.py without the first column.
+    """
+    df = pd.read_csv(dataframe, header=None)
+    processed_csv = df.drop(df.columns[0], axis=1)
+    return processed_csv
+
+def train_model(dataframe):
+    """
+    Using logistic regression to train model taking x feature values and y target values.
+    """
+    x = dataframe.iloc[:, :-1]
+    y = dataframe.iloc[:, -1]
+    train = LogisticRegression(solver="lbfgs", multi_class="multinomial")
+    model = train.fit(x, y)
+
+    return model
+
+def pickle(model):
+    pickle.dump(model, open(args.modelfile, 'wb'))
 
 parser = argparse.ArgumentParser(description="Train a maximum entropy model.")
 parser.add_argument("-N", "--ngram", metavar="N", dest="ngram", type=int, default=3, help="The length of ngram to be considered (default 3).")
@@ -22,6 +44,10 @@ args = parser.parse_args()
 print("Loading data from file {}.".format(args.datafile))
 print("Training {}-gram model.".format(args.ngram))
 print("Writing table to {}.".format(args.modelfile))
+
+dataframe = read_file(args.datafile)
+model = train_model(dataframe)
+pickle(model)
 
 # YOU WILL HAVE TO FIGURE OUT SOME WAY TO INTERPRET THE FEATURES YOU CREATED.
 # IT COULD INCLUDE CREATING AN EXTRA COMMAND-LINE ARGUMENT OR CLEVER COLUMN
