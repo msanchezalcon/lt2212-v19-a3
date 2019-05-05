ae19ec2b45e74a612615c0daa1bbdbe30c0413e6
Míriam Sánchez-Alcón <46067629+msanchezalcon@users.noreply.github.com>
2019-05-05 12:53:58 +0200

    Update test.py

--- test.py
+++ test.py
@@ -10,6 +10,8 @@ from sklearn.linear_model import LogisticRegression
 
 # add whatever additional imports you may need here.
 
+
+
 def read_file(datafile):
     """
     Opens csv file created in gendata.py without the first column.
@@ -30,17 +32,38 @@ def open_model(modelfile):
     return model
 
 
-def vectors_labels(dataframe):
+def vectors_labels(datafile):
     """
-    Gets the vectors (columns, except last one containing the labels and  class labels.
+    Gets the vectors (columns, except last one containing the labels and  class labels).
     """
-    vectors = dataframe.iloc[:, :-1]
-    labels = dataframe.iloc[:, -1]
+    vectors = datafile.iloc[:, :-1]
+    labels = datafile.iloc[:, -1]
 
     return vectors, labels
-  
-  
-  # IN PROGRESS !!!
+
+def test_model(vectors, labels, model):
+    """
+    Getting  accuracy and perplexity values from vectors and labels.
+    """
+    overall_prob = []
+
+    pred = model.predict(vectors)
+    accuracy = model.score(vectors, labels)
+    log_prob = model.predict_log_proba(vectors)
+
+    for index in range(len(vectors)):
+        vec = vectors.iloc[index]
+        predict_probab = model.predict_proba([vec])[0]
+        max_prob = max(predict_probab)
+        overall_prob.append(max_prob)
+
+    entr = sum(overall_prob) / len(overall_prob)
+    perp = 2**entr
+
+    print("Accuracy: " + accuracy)
+    print("Perplexity: " + perp)
+
+
 
 parser = argparse.ArgumentParser(description="Test a maximum entropy model.")
 parser.add_argument("-N", "--ngram", metavar="N", dest="ngram", type=int, default=3, help="The length of ngram to be considered (default 3).")
@@ -58,3 +81,9 @@ print("Testing {}-gram model.".format(args.ngram))
 
 print("Accuracy is ...")
 print("Perplexity is...")
+
+
+datafile = read_file(args.datafile)
+model = open_model(args.modelfile)
+vectors, labels = vectors_labels(datafile)
+test_model(vectors, labels, model)
